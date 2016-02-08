821a00f8cb01d9c717c2097b73459ab77a1b8b40
Hackerton <alanlamkenlun@hotmail.com>
2016-02-08 12:26:00 +0800

    The lastest changes

--- snapshot.sh
+++ snapshot.sh
@@ -3,8 +3,4 @@
 pool=$1
 dataset=$2
 
-<<<<<<< HEAD
 sudo zfs snapshot $pool/$dataset@ZFS_$(date +%Y-%m-%d-%H%M)
-=======
-sudo zfs snapshot $pool/$dataset@ZFS_$(date +%Y-%m-%d-%H%M)
->>>>>>> d172b7b1c595e01e39cea53ca11f2173a3b451b4
