06e382d6662f02ddf04f082e7cade14162b9a0f8
hackerton <alanlamkenlun@hotmail.com>
2017-04-20 22:05:55 +0800

    Added reverse zfs snapshot destruction for improve performance

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for snapshot in `sudo zfs list -H -t snapshot | cut -f 1` 
+for snapshot in `sudo zfs list -H -t snapshot | cut -f 1 | tac` 
 do 
  echo $snapshot " DONE" 
  sudo zfs destroy $snapshot
